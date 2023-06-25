7c2c58daca5a822b477bec59b69b15862b5c6473
marvinobig <88102362+marvinobig@users.noreply.github.com>
2023-06-25 09:06:25 +0100

    add alert functionality

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,6 +2,29 @@
 
 public partial class MainPage : ContentPage
 {
-	
+	public MainPage() { 
+		InitializeComponent();
+	}
+
+	private async void ButtonAlert(object sender, EventArgs e)
+	{
+		Button btn = (Button)sender;
+
+		switch (btn.Text)
+		{
+			case "Addition":
+				await DisplayAlert("Game Chosen", "Addition", "Play");
+				break;
+			case "Subtraction":
+                await DisplayAlert("Game Chosen", "Subtraction", "Play");
+                break;
+            case "Multiplication":
+                await DisplayAlert("Game Chosen", "Multiplication", "Play");
+                break;
+            case "Division":
+                await DisplayAlert("Game Chosen", "Division", "Play");
+                break;
+        }
+	}
 }
 
