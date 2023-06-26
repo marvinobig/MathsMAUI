8e2e80d32722aad5e26ffd9f22aee3b7aaf73e91
marvinobig <88102362+marvinobig@users.noreply.github.com>
2023-06-26 10:47:41 +0100

    dependency inject database repository

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -6,6 +6,7 @@ public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
 	{
+		string dbPath = Path.Combine(FileSystem.AppDataDirectory, "Math.db");
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
@@ -13,7 +14,9 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+			.Services
+			.AddSingleton(s => ActivatorUtilities.CreateInstance<GameHistory>(s, dbPath));
 
 #if DEBUG
 		builder.Logging.AddDebug();
