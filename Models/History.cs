34b92cf6e482ad404e6659635aaf0e3c041ea400
marvinobig <88102362+marvinobig@users.noreply.github.com>
2023-06-26 10:48:25 +0100

    create history data model

---
+++ Models/History.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SQLite;
+
+namespace MathsMAUI.Models
+{
+    [Table("history")]
+    public class History
+    {
+        [PrimaryKey, AutoIncrement, Column("id")]
+        public int id {  get; set; }
+        public MathOperation gameChoice { get; set; }
+        public string question { get; set; }
+        public int answer { get; set; }
+        public int userAnswer { get; set; }
+        public DateTime playedAt { get; set; }
+    }
+
+    public enum MathOperation
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division,
+    }
+}
