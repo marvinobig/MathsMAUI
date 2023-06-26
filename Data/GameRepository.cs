ef1faeb29a303c2725ad6b38971fcecd027fd4ff
marvinobig <88102362+marvinobig@users.noreply.github.com>
2023-06-26 10:48:12 +0100

    create data handler class

---
+++ Data/GameRepository.cs
@@ -0,0 +1,33 @@
+﻿using MathsMAUI.Models;
+using SQLite;
+
+namespace MathsMAUI.Data
+{
+    public class GameRepository
+    {
+        private readonly string _dbPath;
+        private readonly SQLiteConnection _dbConn;
+
+        public GameRepository(string dbPath)
+        {
+            _dbPath = dbPath;
+            _dbConn = new SQLiteConnection(_dbPath);
+            _dbConn.CreateTable<History>();
+        }
+
+        public List<History> GetAllHistory()
+        {
+            return _dbConn.Table<History>().ToList();
+        }
+        
+        public void AddHistory(History history)
+        {
+            _dbConn.Insert(history);
+        }
+
+        public void DeleteHistory(int id) 
+        { 
+            _dbConn.Delete(id);
+        }
+    }
+}
